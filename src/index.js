16247bc86b47605764352acaa82be28dd8a00fa8
Vladimir <vova3.4.99@gmail.com>
2022-02-15 11:49:34 +0300

    create algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
-// You should implement your task here.
-
+let string = [];
 module.exports = function towelSort (matrix) {
-  return [];
+    for (let i = 0; i < matrix.length; i++){
+        if (i % 2 !== 0){
+            let reverse = matrix[i].reverse();
+            reverse.forEach(item => string.push(item))
+        }else {
+            matrix[i].forEach(item => string.push(item))
+        }
+    }
+  return string;
 }
